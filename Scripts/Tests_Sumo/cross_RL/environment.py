ce8a3039ab6c4afe0ceabc97de34578b88fc9ecc
Sebastian Wolf <sebxwolf@gmail.com>
2019-05-28 23:37:49 +0200

    - added reward history in tf summary writer

--- Scripts/Tests_Sumo/cross_RL/environment.py
+++ Scripts/Tests_Sumo/cross_RL/environment.py
@@ -178,7 +178,7 @@ class Env:
         # b = np.round(state,decimals=1)
         # aux = np.divide(a, b, out=np.zeros_like(a), where=b!=0)
 
-        return np.sign(-np.sum(diff)) # delta waiting time in the network
+        return np.sum(diff) # delta waiting time in the network
 
     def compute_waiting_time(self):
         aux= []
@@ -203,6 +203,7 @@ class Env:
         wt = self.compute_waiting_time()
 
         self.take_action(action)
+
         self.connection.simulationStep(self.connection.simulation.getTime() + self.time_step) # Run the simulation time_step (s)
         self.state.update_state(connection = self.connection)
         next_state = self.state.get()
